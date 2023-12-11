107fa2c5b7ba1baab4e19fbb3f732909e9928d01
tvankith <tvankith@gmail.com>
2023-12-11 23:23:16 +0530

    source code link added

--- app/page.tsx
+++ app/page.tsx
@@ -2,12 +2,22 @@
 import React from 'react';
 import { Hero } from './Hero';
 import { Page } from './components/Page';
+import { ptSansNarrow } from './fonts';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faGithub } from '@fortawesome/free-brands-svg-icons'
+import Link from 'next/link';
 
 export default function Home() {
 
   return (
     <Page>
       <Hero />
+      <Link href={"https://github.com/tvankith/portfolio"}>
+        <div className='fixed bottom-5 w-full flex flex-row gap-x-1 justify-center items-center underline'>
+          <FontAwesomeIcon className='text-black text-[14px] md:text-[20px]' icon={faGithub} />
+          <p className={`text-[18px]  ${ptSansNarrow.className}`}>Built with Next.js</p>
+        </div>
+      </Link>
     </Page>
   )
 }
