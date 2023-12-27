c80ed7379830d8b1dd2d98c5d6371aea734c4128
tvankith <tvankith@gmail.com>
2023-12-27 19:55:42 +0530

    build issue fixed

--- app/Hero.tsx
+++ app/Hero.tsx
@@ -13,13 +13,13 @@ export const Hero = () => {
         <Container>
           <div className=' text-black flex flex-col pt-[25vh]'>
             <h1 className={`lg:leading-[100px] ${playfair.className} text-secondary font-black lg:text-[112px] text-[50px] leading-tight`}>
-              Hi, I'm Ankith
+              Hi, I&apos;m Ankith
             </h1>
             <h3 className={`${ptSansNarrow.className} lg:leading-[40px] text-secondary font-black text-[20px] leading-normal md:text-[30px]`}>
               A skilled web developer creating great online experiences
             </h3>
             <h5 className={`${ptSansNarrow.className} lg:leading-[40px] text-secondary font-black text-[20px] leading-normal md:text-[30px]`}>
-              Let's build something amazing for the web!
+              Let&apos;s build something amazing for the web!
             </h5>
             <a
               className={`bg-accent text-primary rounded-md mt-[22px] py-[12px] px-[20px] ${playfair.className} text-[20px] w-fit cursor-pointer`}
